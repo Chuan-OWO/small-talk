1a3c9f5aa405c6b3882bcd0bd8235810a3763ea9
hsu <chihchuan.hsu316@gmail.com>
2022-03-31 13:59:01 +0800

    動態生成網頁

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 //從第一支程式 所以JS匯入 你要的檔案 =>進入點
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path"); //匯入path 模組
 console.log(__dirname);//確認進入點路徑
 //require 匯入匯出語法 path node.js語法  path 路徑的意思
 
+
+
 module.exports = {
     entry:'./main.js',//進入點
     output:{
-        filename:'abc.js',
+        filename:'bundle.[hash].js',
         //_dirname 取得程式執行位置的實際路徑
         //C:\Users\iii\20220330\small-talk (開始)
-        path: path.resolve(__dirname,'public')
+        path: path.resolve(__dirname,'dist')
         //C:\Users\iii\20220330\small-talk\dist (結果)
-    }
+    },
+    Plugins:[new HtmlWebpackPlugin()]
 }
