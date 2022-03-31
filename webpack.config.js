778a243dfddca055c19c2944163028ec517b2163
hsu <chihchuan.hsu316@gmail.com>
2022-03-31 15:23:57 +0800

    安裝 mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,45 @@
 //從第一支程式 所以JS匯入 你要的檔案 =>進入點
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path"); //匯入path 模組
 console.log(__dirname);//確認進入點路徑
 //require 匯入匯出語法 path node.js語法  path 路徑的意思
 
 module.exports = {
-    entry:'./main.js',//進入點
-    output:{
-        filename:'bundle.[hash].js',
+    entry: './main.js',//進入點
+    output: {
+        filename: 'bundle.[hash].js',
         //_dirname 取得程式執行位置的實際路徑
         //C:\Users\iii\20220330\small-talk (開始)
-        path: path.resolve(__dirname,'dist')
+        path: path.resolve(__dirname, 'dist')
         //C:\Users\iii\20220330\small-talk\dist (結果)
     },
-    plugins:[new HtmlWebpackPlugin({
-        template:'./template.html', //指定檔案
-        //filename:'./site.html', //設定成這個檔案名字
-        abc:'webpack demo123', //
-        inject:'body'
-    })],
-    devServer:{
-        port:9000,
-        open:true
+    plugins:
+        [new HtmlWebpackPlugin({
+            template: './template.html', //指定檔案
+            //filename:'./site.html', //設定成這個檔案名字
+            abc: 'webpack demo123', //
+            inject: 'body'
+        }),
+    new MiniCssExtractPlugin({
+        filename:'main.[hash].css'
+    })
+    ],
+    devServer: {
+        port: 9000,
+        open: true
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/i,
-                use:["style-loader","css-loader"]
+                test: /\.css$/i,
+                //use: ["style-loader", "css-loader"]
+                //<sytle></style>
+                //執行時由後往前執行
+                use:[MiniCssExtractPlugin.loader,"css-loader"]
+                //<link rel="styleshess" href="/.css">
             }
         ]
     }
-    
+
 }
