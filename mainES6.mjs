fa3bfcaf95d6ea55067733805206a51b4d6df6eb
hsu <chihchuan.hsu316@gmail.com>
2022-03-31 10:35:37 +0800

    新增 輸入innerHTML

--- mainES6.mjs
+++ mainES6.mjs
@@ -1,7 +1,8 @@
 //ES6 匯入
 import {person} from './personES6.mjs'
 console.log(`${person.fN} ${person.lN}`)
-
+const myDiv = document.querySelector('#div1');
+myDiv.innerHTML = `<h2>${person.fN} ${person.lN}</h2>`;
 //執行 node main.js
 //開發階段希望模組化 提升編輯程式效率
 //執行時希望打包成一包 提高網頁效能
